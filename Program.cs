1af0bd85575d25f4c406d6dc397cd7c25068952b
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-03 13:25:09 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        enum Months
+        {
+                Jan = 1,
+                Feb,
+                Mar,
+                Apr,
+                May,
+                Jun,
+                Jul,
+                Aug,
+                Sep,
+                Oct,
+                Nov,
+                Dec,
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int myBirthMonth = (int)Months.Nov;
+            int myBirthDay = 23;
+            int myBirthYear = 2003;
+
+            string myBirthDate = $"My Birthday is on {myBirthMonth}/{myBirthDay}/{myBirthYear}";
         }
     }
 }
